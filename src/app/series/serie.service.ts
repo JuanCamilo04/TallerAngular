6ed63af6a7430db3525488fabfc350b8c5d87bdb
JuanCamilo04 <jc.caceresp1@uniandes.edu.co>
2025-11-09 19:23:07 -0500

    correcciones

--- src/app/series/serie.service.ts
+++ src/app/series/serie.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Serie } from './serie';
-import { dataSeries } from './dataSeries';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SerieService {
-  constructor() {}
+  private apiUrl = 'https://gist.githubusercontent.com/josejbocanegra/8490b48961a69dcd2bfd8a360256d0db/raw/34ff30dbc32392a69eb0e08453a3fc975a3890f0/series.json';
+
+  constructor(private http: HttpClient) {}
 
   getSeries(): Observable<Serie[]> {
-    return of(dataSeries);
+    return this.http.get<Serie[]>(this.apiUrl);
   }
 }
